64a4103879b7ca3f23dcb3a3faae95560367c887
kripansh8981 <168976331+kripansh8981@users.noreply.github.com>
2025-07-27 21:31:11 +0530

    removed the api_key

--- weather_project_react/src/SearchBox.jsx
+++ weather_project_react/src/SearchBox.jsx
@@ -7,7 +7,7 @@ export default function SearchBox({ updateInfo }) {
   let [city, setCity] = useState("");
   let [error, setError] = useState(false);
   const API_URL = "https://api.openweathermap.org/data/2.5/weather";
-  const API_KEY = "2acce3453cae3149d7ba5e18f4e58306";
+ // here api key is removed for safety purpose
 
   let getWeatherInfo = async () => {
     try {
